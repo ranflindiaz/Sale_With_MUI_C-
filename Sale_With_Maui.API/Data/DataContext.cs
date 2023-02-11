40dd1c2e182e612e7b654679610033a8970e2ca1
Ranflin Diaz <54969258+ranflindiaz@users.noreply.github.com>
2023-02-10 23:48:52 -0500

    Add Data Context

---
+++ Sale_With_Maui.API/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sale_With_Maui.Shared.Entities;
+
+namespace Sale_With_Maui.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
---
+++ Sale_With_Maui.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sale_With_Maui.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener mas de {1} caractéres.")]
+        public string Name { get; set; } = null!;
+    }
+}
