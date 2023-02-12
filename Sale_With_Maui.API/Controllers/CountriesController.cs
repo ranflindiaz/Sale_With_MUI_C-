acb317a88125122ee8ea20780dde8504ce4ea398
Ranflin Diaz <54969258+ranflindiaz@users.noreply.github.com>
2023-02-12 00:38:04 -0500

    GET,ADD, PUT, DELETE in API project Completed

--- Sale_With_Maui.API/Controllers/CountriesController.cs
+++ Sale_With_Maui.API/Controllers/CountriesController.cs
@@ -1,13 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sale_With_Maui.API.Data;
 using Sale_With_Maui.Shared.Entities;
 
 namespace Sale_With_Maui.API.Controllers
 {
-    [Route("/api/countries")]
     [ApiController]
+    [Route("/api/countries")]
     public class CountriesController : ControllerBase
     {
         private readonly DataContext _dataContext;
@@ -29,6 +28,41 @@ namespace Sale_With_Maui.API.Controllers
             _dataContext.Countries.Add(country);
             await _dataContext.SaveChangesAsync();
             return Ok(country);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var country = await _dataContext.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(country);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Country country)
+        {
+            _dataContext.Update(country);
+            await _dataContext.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delelte(int id)
+        {
+            var afectedRows = await _dataContext.Countries
+                .Where(x => x.Id == id)
+                .ExecuteDeleteAsync();
+
+            if (afectedRows == 0)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
         }
     }
 }
