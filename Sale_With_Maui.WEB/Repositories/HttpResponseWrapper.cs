26265dc06fa57c62375e8151e8ae1af940d1093a
Ranflin Diaz <54969258+ranflindiaz@users.noreply.github.com>
2023-02-27 22:31:05 -0500

    Finished Countries CRUD

--- Sale_With_Maui.WEB/Repositories/HttpResponseWrapper.cs
+++ Sale_With_Maui.WEB/Repositories/HttpResponseWrapper.cs
@@ -17,7 +17,7 @@ namespace Sale_With_Maui.WEB.Repositories
 
         public HttpResponseMessage HttpResponseMessage { get;set; }
 
-        public async Task<string?> GetErrorMessage()
+        public async Task<string?> GetErrorMessageAsync()
         {
             if (!Error) { return null; }
 
