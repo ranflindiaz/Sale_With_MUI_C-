998623a169a59d18a6afec338b9f56710fa53a27
Ranflin Diaz <54969258+ranflindiaz@users.noreply.github.com>
2023-03-11 09:46:54 -0500

    Add Security hardcode on Web

---
+++ Sale_With_Maui.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sale_With_Maui.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var ralphUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Ranflin"),
+                new Claim("LastName", "Diaz"),
+                new Claim(ClaimTypes.Name, "ranflin@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            },
+            authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+
+        }
+    }
+}
--- Sale_With_Maui.WEB/Program.cs
+++ Sale_With_Maui.WEB/Program.cs
@@ -1,7 +1,9 @@
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sale_With_Maui.WEB;
+using Sale_With_Maui.WEB.Auth;
 using Sale_With_Maui.WEB.Interfaces;
 using Sale_With_Maui.WEB.Repositories;
 
@@ -9,8 +11,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7298/") });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7298/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
+
 
 await builder.Build().RunAsync();
