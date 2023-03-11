dc52cd549ea961144a7edc8d4e7f754f0dc732b0
Ranflin Diaz <54969258+ranflindiaz@users.noreply.github.com>
2023-03-11 09:57:42 -0500

    Update

--- Sale_With_Maui.WEB/Auth/AuthenticationProviderTest.cs
+++ Sale_With_Maui.WEB/Auth/AuthenticationProviderTest.cs
@@ -16,7 +16,7 @@ namespace Sale_With_Maui.WEB.Auth
                 new Claim(ClaimTypes.Role, "Admin")
             },
             authenticationType: "test");
-            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(ralphUser)));
 
         }
     }
